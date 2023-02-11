cb519ee47fb4a6d3f6fd9d590ac06e436a18297c
venkatasrilakshmic <pulugu.vsrilakshmi@gmail.com>
2023-02-11 20:59:58 +0000

    Commit with sort operation

--- PySpark/PySpark_Code1.py
+++ PySpark/PySpark_Code1.py
@@ -5,6 +5,7 @@ findspark.init()
 from pyspark.sql import SparkSession
 #Importing PySpark types for DataFrame use
 from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 # Creating the SparkSession in Local Machine with 2 Treads
 spark = SparkSession.builder.master("local[2]").appName('SampleApp').getOrCreate()
@@ -20,12 +21,22 @@ myList = [Row('Siva',1000,10000.00),
 # Creating the Explicit Schema for the List of Rows
 mySchema = """Name string, id int, salary float"""
 
-#Create DataFrame using the above list and schema
+#Create DataFrame using the above list and schema and Transform the data
 df_list = spark.createDataFrame(myList,mySchema)
-df_list.show()
+df1 = df_list.select("*",lit(current_timestamp()).alias('TimeStamp'))
+df1.withColumn('rownumber',lit(monotonically_increasing_id()))
+# Get the  Number of Partitions before Repartition
+print(df1.rdd.getNumPartitions())
+
+df2 = df1.coalesce(1).sort(col('salary').desc())
+
+# Get the  Number of Partitions After Repartition
+print(df2.rdd.getNumPartitions())
+
+df2.show()
 
 #Provide the explain plan of written code
-df_list.explain(extended=True)
+df2.explain(extended=True)
 
 #Stop the Spark Session
 spark.stop()
