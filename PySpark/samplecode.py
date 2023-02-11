30888fb51857651ba00e91a781d24040610bab7e
venkatasrilakshmic <pulugu.vsrilakshmi@gmail.com>
2023-02-11 20:26:41 +0000

    Commit 2

---
+++ PySpark/samplecode.py
@@ -0,0 +1,31 @@
+# locate the Spark installation
+import findspark
+findspark.init() 
+#Importing Spark Session
+from pyspark.sql import SparkSession
+#Importing PySpark types for DataFrame use
+from pyspark.sql.types import *
+
+# Creating the SparkSession in Local Machine with 2 Treads
+spark = SparkSession.builder.master("local[2]").appName('SampleApp').getOrCreate()
+
+#The following command provides the SparkUI Details in the PySpark application
+print(spark.sparkContext.uiWebUrl)
+
+#Creating list of Rows for creating the DataFrame
+myList = [Row('Siva',1000,10000.00),
+          Row('Krishna',2000,200000.00),
+          Row('yash',500,10000.00)]
+
+# Creating the Explicit Schema for the List of Rows
+mySchema = """Name string, id int, salary float"""
+
+#Create DataFrame using the above list and schema
+df_list = spark.createDataFrame(myList,mySchema)
+
+#Provide the explain plan of written code
+df_list.explain(extended=True)
+
+#Stops the Spark Session
+spark.stop()
+
